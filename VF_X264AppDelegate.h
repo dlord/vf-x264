bb3dd2ea633cac38a116d0ae74a286b1511b469a
John Paul Alcala <ore.wa.dragon.lord@gmail.com>
2010-10-29 20:39:48 +0800

    Added initial project artifacts.
    Signed-off-by: John Paul Alcala <ore.wa.dragon.lord@gmail.com>

---
+++ VF_X264AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  VF_X264AppDelegate.h
+//  VF-X264
+//
+//  Created by John Paul Alcala on 10/29/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VF_X264AppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ VF_X264AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  VF_X264AppDelegate.m
+//  VF-X264
+//
+//  Created by John Paul Alcala on 10/29/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "VF_X264AppDelegate.h"
+
+@implementation VF_X264AppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
