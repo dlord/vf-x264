bb85e965fd1488bc98014a8332d31148826436d4
John Paul Alcala <ore.wa.dragon.lord@gmail.com>
2010-10-31 18:37:23 +0800

    Added Constants file.
    Signed-off-by: John Paul Alcala <ore.wa.dragon.lord@gmail.com>

---
+++ Constants.h
@@ -0,0 +1,19 @@
+//
+//  Constants.h
+//  VF-X264
+//
+//  Created by John Paul Alcala on 10/30/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+extern NSString * const X264_BASELINE;
+extern NSString * const X264_MAIN;
+extern NSString * const X264_HIGH;
+
+extern NSString * const IPOD;
+extern NSString * const IPHONE;
+extern NSString * const PS3;
+extern NSString * const PSP;
+extern NSString * const PHONE;
---
+++ Constants.m
@@ -0,0 +1,19 @@
+//
+//  Constants.m
+//  VF-X264
+//
+//  Created by John Paul Alcala on 10/30/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "Constants.h"
+
+NSString * const X264_BASELINE=@"BASELINE";
+NSString * const X264_MAIN=@"MAIN";
+NSString * const X264_HIGH=@"HIGH";
+
+NSString * const IPOD=@"iPod";
+NSString * const IPHONE=@"iPhone";
+NSString * const PS3=@"PS3";
+NSString * const PSP=@"PSP";
+NSString * const PHONE=@"Generic Phone";
